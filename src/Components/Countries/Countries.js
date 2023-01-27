f1b7c1b2e7da45c8a8c3dda9ca65d9da82e56694
sabidalam <Shafkatsaad@gmail.com>
2023-01-27 21:28:16 +0600

    done

---
+++ src/Components/Countries/Countries.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import Country from './Country';
+import './Countries.css';
+
+const Countries = () => {
+    const [countries, setCountries] = useState([]);
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+            .then(res => res.json())
+            .then(data => setCountries(data))
+    }, [])
+    return (
+        <div>
+            <h2>Visiting Countries</h2>
+            <div className='countries-container'>
+                {
+                    countries.map(country => <Country country={country}></Country>)
+                }
+            </div>
+
+        </div>
+    );
+};
+
+export default Countries;
---
+++ src/Components/Countries/Country.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Country.css'
+const Country = (props) => {
+    const { name, area, region, flags } = props.country;
+    return (
+        <div className='country'>
+            <h2>Country Name: {name.common}</h2>
+            <img src={flags.png} alt="" />
+            <p>Area: {area}</p>
+            <p>Region: {region}</p>
+        </div>
+    );
+};
+
+export default Country;
